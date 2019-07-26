4a49b96a790ee21dc1807e3c1ed662ad69c6265a
Florian Digne <fdigne@me.com>
2019-07-26 22:07:31 +0200

    Add client domain

---
+++ src/main/java/domain/Client.java
@@ -0,0 +1,65 @@
+package domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class Client implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="phone_number")
+    private String phoneNumber;
+
+    public Client() {
+    }
+
+    public Client(String name, String firstName, String phoneNumber) {
+        this.name = name;
+        this.firstName = firstName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
